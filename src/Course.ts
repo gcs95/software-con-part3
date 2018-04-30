7e005fe8a467674aa949a6e75c2b249b6b8774c2
Gaanploo Somboon <gaanploo_somboon@cmu.ac.th>
2018-04-30 18:09:20 +0700

    add get/set

--- src/Course.ts
+++ src/Course.ts
@@ -16,4 +16,52 @@ class Course {
         this.courseTime = courseTime;
     }
   
+		getCourseID(): string {
+        return this.courseID;
+    }
+
+    setCourseID(courseID: string): void {
+            this.courseID = courseID;
+    }
+
+    getCourseName(): string {
+        return this.courseName;
+    }
+
+    setCourseName(courseName: string): boolean {
+        this.courseName = this.courseName;
+    }
+
+    getCourseInstructor(): Instructor {
+        return this.courseInstructor;
+    }
+
+    setCourseInstructor(courseInstructor: Instructor): void {
+        this.courseInstructor = courseInstructor;
+    }
+
+    getCourseCredit(): number {
+        return this.courseCredit;
+    }
+
+    setCourseCredit(courseCredit: number): void {
+        this.courseCredit = courseCredit;
+    }
+
+    getCourseDays(): Day[] {
+        return this.courseDays;
+    }
+
+    setCourseDays(courseDays: Day[]): void {
+        this.courseDays = courseDays;
+    }
+
+    getCourseTime(): Time {
+        return this.courseTime;
+    }
+
+    setCourseTime(courseTime: Time): void {
+        this.courseTime = courseTime;
+    }
+  
 }
