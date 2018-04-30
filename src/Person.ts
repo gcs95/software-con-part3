7fd473bf229a408c8efba371a41c24d2234b185b
Ponpipat Yodkanto <inLamai@Ponpipats-MacBook-Air.local>
2018-04-30 13:58:16 +0700

    add constructor for person

--- src/Person.ts
+++ src/Person.ts
@@ -1,9 +1,17 @@
-class Person{
-    
+class Person {
+
     private firstName: string;
     private lastName: string;
     private gender: Gender;
     private username: string;
     private password: string;
+
+    constructor(firstName: string, lastName: string, gender: Gender, username: string, password: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.gender = gender;
+    }
 
 }
