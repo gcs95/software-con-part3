2a7fb95a21183e97e184b98772b27f39a14c73a3
Gaanploo Somboon <gaanploo_somboon@cmu.ac.th>
2018-04-30 17:55:53 +0700

    course class and attributes

---
+++ src/Course.ts
@@ -0,0 +1,10 @@
+class Course {
+
+    private courseID: string;
+    private courseName: string;
+    private courseInstructor: Instructor;
+    private courseCredit: number;
+    private courseDays: Day[];
+    private courseTime: Time;
+  
+}
