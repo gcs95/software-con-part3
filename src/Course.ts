509cd1c77eb719f4900d86a4aded1c2ca6eac318
Gaanploo Somboon <gaanploo_somboon@cmu.ac.th>
2018-04-30 18:07:07 +0700

    add constructor

--- src/Course.ts
+++ src/Course.ts
@@ -7,4 +7,13 @@ class Course {
     private courseDays: Day[];
     private courseTime: Time;
   
+  	constructor(courseID: string, courseName: string, courseInstructor: Instructor, courseCredit: number, courseDays: Day[], courseTime: Time) {
+        this.courseID = courseID;
+        this.courseName = courseName;
+        this.courseInstructor = courseInstructor;
+        this.courseCredit = courseCredit;
+        this.courseDays = courseDays;
+        this.courseTime = courseTime;
+    }
+  
 }
