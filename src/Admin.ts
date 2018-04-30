538a0dbe8a0c9d9cd4f562e3164c390be915d7c4
Piyawasin Pikror-ngarn <piyawasin@gmail.com>
2018-04-30 14:16:16 +0700

    Implemented Admin class.

---
+++ src/Admin.ts
@@ -0,0 +1,7 @@
+class Admin extends Person {
+
+    constructor(firstName: string, lastName: string, gender: Gender, username: string, password: string) {
+        super(firstName, lastName, gender, username, password)
+    }
+
+}
